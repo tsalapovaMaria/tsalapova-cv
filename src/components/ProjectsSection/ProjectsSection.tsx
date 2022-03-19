9a4f0240651a9d760f824103cba7ca042fa76859
maria.tsalapova <Mariya.Tsalapova@x5.ru>
2022-03-19 22:30:47 +0300

    Remove projects pictures

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -18,7 +18,7 @@ const ProjectsSection = () => {
                             <div className="skills-content">
                                 <div className="row align-items-center flex-column">
                                     <div className="col-12 px-5">
-                                        <h3 className="skills-list-title text-xl-left text-center">junior frontend developer</h3>
+                                        <h3 className="skills-list-title text-xl-left text-center">frontend developer</h3>
                                     </div>
                                     <div className="col-xl-12 col-md-6 col-12 mx-xl-auto px-xl-5 px-md-3 px-5">
                                         <ul className="skills-list text-left">
@@ -26,44 +26,6 @@ const ProjectsSection = () => {
                                             <li className="skills-item"><span><a href="http://tsalapova.ru/" rel="noreferrer noopener" target="_blank">CV-site</a> creation :)</span></li>
                                             <li className="skills-item"><span><a href="https://tsalapovamaria.github.io/new-shares-app/" rel="noreferrer noopener" target="_blank">Shares app</a> development based on Figma layout (on Russian)</span></li>
                                         </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-12">
-                            <div className="row justify-content-center">
-                                <div className="col-md-8">
-                                    <div className="row mb-4">
-                                        <a href="../../images/centrchpu/index.png" data-toggle="lightbox"
-                                           data-gallery="example-gallery"
-                                           className="col-sm-4">
-                                            <img src={firstImg} className="img-fluid"/>
-                                        </a>
-                                        <a href="../../images/centrchpu/katalog.png" data-toggle="lightbox"
-                                           data-gallery="example-gallery"
-                                           className="col-sm-4">
-                                            <img src={secImg} className="img-fluid"/>
-                                        </a>
-                                        <a href="../../images/centrchpu/services.png" data-toggle="lightbox"
-                                           data-gallery="example-gallery"
-                                           className="col-sm-4">
-                                            <img src={thirdImg} className="img-fluid"/>
-                                        </a>
-                                    </div>
-                                    <div className="row mb-4">
-                                        <a href="../../images/centrchpu/materials.png" data-toggle="lightbox"
-                                           data-gallery="example-gallery"
-                                           className="col-sm-4">
-                                            <img src={forthImg} className="img-fluid"/>
-                                        </a>
-                                        <a href="../../images/centrchpu/articles.png" data-toggle="lightbox"
-                                           data-gallery="example-gallery"
-                                           className="col-sm-4">
-                                            <img src={fifthImg} className="img-fluid"/>
-                                        </a>
-                                        <a href="../../images/centrchpu/contacts.png" data-toggle="lightbox" data-gallery="example-gallery" className="col-sm-4">
-                                            <img src={sixthImg} className="img-fluid"/>
-                                        </a>
                                     </div>
                                 </div>
                             </div>
